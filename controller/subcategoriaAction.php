263591ff605f8f21504bd95c484ff6bf4cf33ced
gonzalodr <129683931+gonzalodr@users.noreply.github.com>
2024-09-08 18:52:29 -0600

    cambios subcategoria

--- controller/subcategoriaAction.php
+++ controller/subcategoriaAction.php
@@ -2,63 +2,46 @@
     require_once __DIR__ . '/../service/subcategoriaBusiness.php';
     require_once __DIR__ . '/../utils/Utils.php';
 
-    // Función para validar los datos del impuesto
-    function validarDatos($nombre) {
-        $errors = [];
-        if(empty($nombre) || is_numeric($nombre) || $nombre == null){
-            $errors[] = "¡El campo 'Nombre' no puede estar vacio o ser numerico!";
-        }    
-        return $errors;
-    }
-    
     $response = [];
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $accion = $_POST['accion'];
-
         $id_subcategoria = isset($_POST['id']) ? $_POST['id'] : null;
-        $nombre_subcategoria = $_POST['nombre'];
+        $nombre_subcategoria = isset($_POST['nombre']) ? $_POST['nombre']: "";
+        $descripcion = isset($_POST['descripcion'])?$_POST['descripcion']:"";
 
         $subcategoriaBusiness = new SubcategoriaBusiness();
+        $subcategoria = new Subcategoria($nombre,$descripcion,$id_subcategoria);
 
-        if ($accion == 'eliminar') {
-            if (empty($id_subcategoria) || !is_numeric($id_subcategoria)) {
+        switch($accion){
+            case 'eliminar':
+                    $response = $subcategoriaBusiness->deleteSubcategoria($subcategoria);
+                    break;
+            case 'insertar':
+                    $response = $subcategoriaBusiness->insertSubcategoria($subcategoria);
+                    break;
+            case 'actualizar':
+                    $response = $subcategoriaBusiness->updateSubcategoria($subcategoria);
+                    break;
+            default:
                 $response['success'] = false;
-                $response['message'] = "El ID no puede estar vacío.";
-            } else {
-                $result = $subcategoriaBusiness->deleteSubcategoria($id_subcategoria);
-                $response['success'] = $result["success"];
-                $response['message'] = $result["message"];
-            }
-        } else {
-            $validationErrors = validarDatos($nombre_subcategoria);
-            if (empty($validationErrors)) {
-                $subcategoria = new Subcategoria($nombre_subcategoria,$id_subcategoria);
-                if ($accion == 'insertar') {
-                    // Utils::writeLog("Action insert: ".$subcategoria->getSubcategoriaNombre()."  ".$subcategoria->getSubcategoriaId());
-                    $result = $subcategoriaBusiness->insertSubcategoria($subcategoria);
-                    $response['success'] = $result["success"];
-                    $response['message'] = $result["message"];
-                } elseif ($accion == 'actualizar') {
-                    // Utils::writeLog("Action update: ".$subcategoria->getSubcategoriaNombre()."  ".$subcategoria->getSubcategoriaId());
-                    $result = $subcategoriaBusiness->updateSubcategoria($subcategoria);
-                    $response['success'] = $result["success"];
-                    $response['message'] = $result["message"];
-                } else {
-                    $response['success'] = false;
-                    $response['message'] = "Acción no válida.";
-                }
-            } else {
-                $response['success'] = false;
-                $response['message'] = implode(' ', $validationErrors);
-            }
+                $response['message'] = "Acción no válida.";
+                break;
         }
-
         header('Content-Type: application/json');
         echo json_encode($response);
         exit();
     }
 
     if ($_SERVER["REQUEST_METHOD"] == "GET") {
+
+        if (isset($_GET['accion']) && $_GET['accion'] === 'listarSubcategorias') {
+            Utils::writeLog("Ingreso ",UTILS_LOG_FILE);
+            $subcategoriaBusiness = new SubcategoriaBusiness();
+            $result = $subcategoriaBusiness->getAllSubcategorias();
+            header('Content-Type: application/json');
+            echo json_encode($result);
+            exit();
+        }
         // Obtener parámetros de la solicitud GET
         $page = isset($_GET['page']) ? intval($_GET['page']) : 1;
         $size = isset($_GET['size']) ? intval($_GET['size']) : 5;
@@ -67,7 +50,7 @@
         // Validar los parámetros
         if ($page < 1) $page = 1;
         if ($size < 1) $size = 5;
-        Utils::writeLog("Ingreso ",UTILS_LOG_FILE);
+        
         $subcategoriaService = new SubcategoriaBusiness();
         $result = $subcategoriaService->getPaginatedSubcategorias($page, $size, $sort);
         
--- service/subcategoriaBusiness.php
+++ service/subcategoriaBusiness.php
@@ -4,20 +4,64 @@
     class SubcategoriaBusiness{
         private $subcategoriaData;
 
+
+        public function verificacionDeDatos($subcategoria, $update = false,$delete = false){
+           try{
+                $id = $subcategoria->getSubcategoriaId();
+                $nombre = $subcategoria->getSubcategoriaNombre();
+                $errors = [];
+                if($delete && (empty($id) || $id <= 0 || !is_numeric($id))){
+                    $errors[] = "El ID de la subcategoria está vacío o no es válido. Revise que este sea un número y que sea mayor a 0";
+                    Utils::writeLog("El ID '[$id]' de la subcategoria no es válido.", BUSINESS_LOG_FILE);   
+
+                } else if($update){
+                        if(empty($id) || $id <= 0 || !is_numeric($id)){
+                            $errors[] = "El ID de la subcategoria está vacío o no es válido. Revise que este sea un número y que sea mayor a 0";
+                            Utils::writeLog("El ID '[$id]' de la subcategoria no es válido.", BUSINESS_LOG_FILE);
+                        }
+                        if(empty($nombre)){
+                            $errors[] = "El Nombre de la subcategoria esta vacia. Revisa que esta ingresando correctamente el nombre.";
+                            Utils::writeLog("El Nombre '[$nombre]' de la subcategoria no es válido.", BUSINESS_LOG_FILE);
+                        }
+                }else{
+                    if(empty($nombre)){
+                        $errors[] = "El Nombre de la subcategoria esta vacia. Revisa que esta ingresando correctamente el nombre.";
+                        Utils::writeLog("El Nombre '[$nombre]' de la subcategoria no es válido.", BUSINESS_LOG_FILE);
+                    }
+                }
+                if (!empty($errors)) {
+                    throw new Exception(implode('<br>', $errors));
+                }
+                return ["is_valid" => true];
+            } catch (Exception $e) {
+                return ["is_valid" => false, "message" => $e->getMessage()];
+            }
+        }
+
         public function __construct(){
             $this->subcategoriaData = new SubcategoriaData();
         }
-        function getAllSubcategorias(){
-            return $this->subcategoriaData->getAllSubcategorias();
-        }
+        
         function insertSubcategoria($subcategoria){
+            $check = $this->verificacionDeDatos($subcategoria);
+            if(!$check['is_valid']){ return $check; }
             return $this->subcategoriaData->insertSubcategoria($subcategoria);
         }
+
         function updateSubcategoria($subcategoria){
-           return $this->subcategoriaData->updateSubcategorias($subcategoria);
+            $check = $this->verificacionDeDatos($subcategoria,true);
+            if(!$check['is_valid']){ return $check; }
+            return $this->subcategoriaData->updateSubcategorias($subcategoria);
         }
-        function deleteSubcategoria($id){
-            return $this->subcategoriaData->deleteSubcategoria($id);
+
+        function deleteSubcategoria($subcategoria){
+            $check = $this->verificacionDeDatos($subcategoria,false,true);
+            if(!$check['is_valid']){ return $check; }
+            return $this->subcategoriaData->deleteSubcategoria($subcategoria->getSubcategoriaId());
+        }
+
+        function getAllSubcategorias(){
+            return $this->subcategoriaData->getAllSubcategorias();
         }
         function getPaginatedSubcategorias($page, $size, $sort = null){
             return $this->subcategoriaData->getPaginatedSubcategorias($page, $size, $sort);
